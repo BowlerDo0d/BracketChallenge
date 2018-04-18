1c07ef5760c3561b4ba5b3b9ff3a43ee6afdf652
Steve Mahony <smahony22@gmail.com>
2018-04-18 17:10:04 -0400

    Added form fields to admin component

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 import { Bracket } from '../models/bracket.model';
 import { BracketMapper } from '../core/bracket/data/bracket-mapper';
@@ -11,6 +12,7 @@ import { KEYS } from '../constants/global.constants';
   styleUrls: ['./admin.component.scss']
 })
 export class AdminComponent implements OnInit {
+  adminForm: FormGroup;
   masterBracket: Bracket;
 
   constructor(private db: AngularFireDatabase) { }
@@ -21,6 +23,47 @@ export class AdminComponent implements OnInit {
 
       this.masterBracket = BracketMapper(bracket);
     });
+
+    this.adminForm = new FormGroup({
+      // Round 1
+      topSeed0000: new FormControl(null, [Validators.pattern(/\d/), Validators.min(0), Validators.max(4)]),
+      bottomSeed0000: new FormControl(null, [Validators.pattern(/\d/), Validators.min(0), Validators.max(4)]),
+      topSeed0001: new FormControl(null, [Validators.pattern(/\d/), Validators.min(0), Validators.max(4)]),
+      bottomSeed0001: new FormControl(null, [Validators.pattern(/\d/), Validators.min(0), Validators.max(4)]),
+      topSeed0100: new FormControl(null, [Validators.pattern(/\d/), Validators.min(0), Validators.max(4)]),
+      bottomSeed0100: new FormControl(null, [Validators.pattern(/\d/), Validators.min(0), Validators.max(4)]),
+      topSeed0101: new FormControl(null, [Validators.pattern(/\d/), Validators.min(0), Validators.max(4)]),
+      bottomSeed0101: new FormControl(null, [Validators.pattern(/\d/), Validators.min(0), Validators.max(4)]),
+      topSeed1000: new FormControl(null, [Validators.pattern(/\d/), Validators.min(0), Validators.max(4)]),
+      bottomSeed1000: new FormControl(null, [Validators.pattern(/\d/), Validators.min(0), Validators.max(4)]),
+      topSeed1001: new FormControl(null, [Validators.pattern(/\d/), Validators.min(0), Validators.max(4)]),
+      bottomSeed1001: new FormControl(null, [Validators.pattern(/\d/), Validators.min(0), Validators.max(4)]),
+      topSeed1100: new FormControl(null, [Validators.pattern(/\d/), Validators.min(0), Validators.max(4)]),
+      bottomSeed1100: new FormControl(null, [Validators.pattern(/\d/), Validators.min(0), Validators.max(4)]),
+      topSeed1101: new FormControl(null, [Validators.pattern(/\d/), Validators.min(0), Validators.max(4)]),
+      bottomSeed1101: new FormControl(null, [Validators.pattern(/\d/), Validators.min(0), Validators.max(4)]),
+      // Round 2
+      topSeed0010: new FormControl(null, [Validators.pattern(/\d/), Validators.min(0), Validators.max(4)]),
+      bottomSeed0010: new FormControl(null, [Validators.pattern(/\d/), Validators.min(0), Validators.max(4)]),
+      topSeed0110: new FormControl(null, [Validators.pattern(/\d/), Validators.min(0), Validators.max(4)]),
+      bottomSeed0110: new FormControl(null, [Validators.pattern(/\d/), Validators.min(0), Validators.max(4)]),
+      topSeed1010: new FormControl(null, [Validators.pattern(/\d/), Validators.min(0), Validators.max(4)]),
+      bottomSeed1010: new FormControl(null, [Validators.pattern(/\d/), Validators.min(0), Validators.max(4)]),
+      topSeed1110: new FormControl(null, [Validators.pattern(/\d/), Validators.min(0), Validators.max(4)]),
+      bottomSeed1110: new FormControl(null, [Validators.pattern(/\d/), Validators.min(0), Validators.max(4)]),
+      // Conference finals
+      topSeed00: new FormControl(null, [Validators.pattern(/\d/), Validators.min(0), Validators.max(4)]),
+      bottomSeed01: new FormControl(null, [Validators.pattern(/\d/), Validators.min(0), Validators.max(4)]),
+      topSeed10: new FormControl(null, [Validators.pattern(/\d/), Validators.min(0), Validators.max(4)]),
+      bottomSeed11: new FormControl(null, [Validators.pattern(/\d/), Validators.min(0), Validators.max(4)]),
+      // Finals
+      topSeed: new FormControl(null, [Validators.pattern(/\d/), Validators.min(0), Validators.max(4)]),
+      bottomSeed: new FormControl(null, [Validators.pattern(/\d/), Validators.min(0), Validators.max(4)])
+    });
+  }
+
+  saveScores() {
+    console.log('data');
   }
 
   showConferenceFinals(conference: number) {
@@ -35,9 +78,8 @@ export class AdminComponent implements OnInit {
       this.masterBracket.conferences[1].winner.name !== null;
   }
 
-  showRound(conference, division, round) {
-    return round === 1 ? true :
-      this.masterBracket &&
+  showQuarterfinals(conference, division) {
+    return this.masterBracket &&
       this.masterBracket.conferences[conference].divisions[division].rounds[1].matchups[0].topSeed.name !== null &&
       this.masterBracket.conferences[conference].divisions[division].rounds[1].matchups[0].bottomSeed.name !== null;
   }
