f9405eb1d1429c31be7693caf535bebbe4bf89d0
BowlerDo0d <smahony39@gmail.com>
2019-04-07 21:40:01 -0400

    Changing the deadline

--- src/app/constants/global.constants.ts
+++ src/app/constants/global.constants.ts
@@ -2,7 +2,7 @@ export const KEYS = {
   DUMMY: '-LA-iP4kxzQ65R1x2ad6',
   MASTER: '-LbtzlBmWSRG9umIzlZ9'
 },
-DEADLINE = new Date('04/20/2019 11:59:59 PM'),
+DEADLINE = new Date('04/14/2019 11:59:59 PM'),
 TEAM_ABBREVIATIONS = {
   Anaheim: 'ANA',
   Boston: 'BOS',
