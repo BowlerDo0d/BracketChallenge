387d1a1947a17c1c72bd2fa3133ddcc3170b1d9b
BowlerDo0d <smahony39@gmail.com>
2021-04-11 22:42:33 -0400

    updated footer to be menu buttons

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { CoreRoutingModule } from './core-routing.module';
 import { FooterComponent } from './footer/footer.component';
@@ -21,6 +22,7 @@ import { HeaderComponent } from './header/header.component';
     MatButtonModule,
     MatIconModule,
     MatMenuModule,
+    MatTabsModule,
     MatToolbarModule
   ],
   exports: [
--- src/app/core/footer/footer.component.ts
+++ src/app/core/footer/footer.component.ts
@@ -7,10 +7,30 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class FooterComponent implements OnInit {
+  activeLink: IMenuItem;
+  menuItems: IMenuItem[];
 
   constructor() { }
 
   ngOnInit(): void {
-  }
+    this.menuItems = [{
+      icon: 'leaderboard',
+      label: 'Scores',
+      link: '/scoreboard'
+    }, {
+      icon: 'help_center',
+      label: 'Help',
+      link: ''
+    }, {
+      icon: 'fingerprint',
+      label: 'Scan',
+      link: ''
+    }, {
+      icon: 'info',
+      label: 'About',
+      link: ''
+    }];
 
+    this.activeLink = this.menuItems[0];
+  }
 }
