45a44695be176e06597dbd8455f0cc461f78b500
Steve Mahony <smahony39@gmail.com>
2024-03-05 20:48:26 -0500

    added angular/fire

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,20 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { FIREBASE_CONFIG } from './constants/firebase.constants';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(
+      provideFirebaseApp(
+        () => initializeApp(FIREBASE_CONFIG)
+      )
+    ),
+    importProvidersFrom(provideAuth(() => getAuth())),
+    importProvidersFrom(provideDatabase(() => getDatabase()))
+  ]
 };
---
+++ src/app/constants/firebase.constants.ts
@@ -0,0 +1,12 @@
+import { FirebaseOptions } from "@angular/fire/app";
+
+export const FIREBASE_CONFIG: FirebaseOptions = {
+    apiKey: "AIzaSyCyydrkmzswz9rkOsNhV4rkrAE9veRB8Og",
+    appId: "1:368571560858:web:e54b7e20aa829e3b38c023",
+    authDomain: "firefly-e2128.firebaseapp.com",
+    databaseURL: "https://firefly-e2128.firebaseio.com",
+    measurementId: "G-9HMEFTVR6Q",
+    messagingSenderId: "368571560858",
+    projectId: "firefly-e2128",
+    storageBucket: "firefly-e2128.appspot.com"
+};
