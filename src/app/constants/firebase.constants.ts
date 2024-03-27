12347ee5ec36cd3c6ee89348099e2a1c9343599a
Steve Mahony <smahony39@gmail.com>
2024-03-27 11:43:35 -0400

    updated firebase settings for new firebase project

--- src/app/constants/firebase.constants.ts
+++ src/app/constants/firebase.constants.ts
@@ -1,12 +1,10 @@
 import { FirebaseOptions } from "@angular/fire/app";
 
 export const FIREBASE_CONFIG: FirebaseOptions = {
-    apiKey: "AIzaSyCyydrkmzswz9rkOsNhV4rkrAE9veRB8Og",
-    appId: "1:368571560858:web:e54b7e20aa829e3b38c023",
-    authDomain: "firefly-e2128.firebaseapp.com",
-    databaseURL: "https://firefly-e2128.firebaseio.com",
-    measurementId: "G-9HMEFTVR6Q",
-    messagingSenderId: "368571560858",
-    projectId: "firefly-e2128",
-    storageBucket: "firefly-e2128.appspot.com"
+    apiKey: "AIzaSyA7UL4JgA5Mkd1-TRXjVM0nTDBQg9wpibg",
+    appId: "1:57858702439:web:9993b33e799f07848968c2",
+    authDomain: "bracket-challenge-83b72.firebaseapp.com",
+    messagingSenderId: "57858702439",
+    projectId: "bracket-challenge-83b72",
+    storageBucket: "bracket-challenge-83b72.appspot.com"
 };
