2e10bd40dba73d497583dac3f1d805730d842c73
BowlerDo0d <smahony39@gmail.com>
2024-06-05 22:46:28 -0400

    swapped models to interfaces

--- src/app/models/round.model.ts
+++ src/app/models/round.model.ts
@@ -1,27 +1,6 @@
 import { Matchup } from './matchup.model';
 
-export class Round {
-  private _matchups: Array<Matchup>;
-  private _name: string;
-
-  constructor(name: string = '', matchups: Array<Matchup> = []) {
-    this._matchups = matchups;
-    this._name = name;
-  }
-
-  get matchups(): Array<Matchup> {
-    return this._matchups;
-  }
-
-  set matchups(matchups: Array<Matchup>) {
-    this._matchups = matchups;
-  }
-    
-  get name(): string {
-      return this._name;
-  }
-
-  set name(name: string) {
-    this._name = name;
-  }
+export interface Round {
+  matchups: Matchup[];
+  name: string;
 }
